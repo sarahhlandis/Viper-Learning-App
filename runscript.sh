c7e2a5564c0f5a69f635e676cf603597fb1ffd5e
sarahhlandis <sarahlandis95@gmail.com>
2022-12-16 18:45:29 +1100

    added venv in runscript in case terminal is closed

--- runscript.sh
+++ runscript.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 if command -v python3 &> /dev/null
-  then python3 src/viper.py
+  then 
+  source venv/bin/activate
+  python3 src/viper.py
 fi
 if command -v python &> /dev/null
-  then python src/viper.py
+  then
+  source venv/bin/activate
+  python src/viper.py
 fi
 
